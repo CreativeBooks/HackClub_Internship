81e67f61bf5089e22ffad5ed87bbbcfbbc9fef9e
AeroBotX <p.garg1606@gmail.com>
2026-03-21 15:25:11 -0400

    Adding project ideas, shop, and overall dashboard

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const starsContainer = document.getElementById('stars');
-for (let i = 0; i < 80; i++) {
+for (let i = 0; i < 300; i++) {
     const star = document.createElement('div');
     star.classList.add('star');
 
-    const size = Math.random() < 0.7 ? 1:2;
+    const size = Math.random() < 0.7 ? 2:3;
     star.style.width = size + 'px';
     star.style.height = size + 'px';
     star.style.top = Math.random() * 100 + '%';
